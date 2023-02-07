c4853a73225c5dfd1222f154c91c5ef3833a0457
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-07 19:07:01 +0700

    finished layout for desktop view

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,7 +4,7 @@ import AccordionItems from "./AccordionItems";
 
 export default function Accordion() {
   return (
-    <div className="mx-auto w-full mt-4 px-6 h-[40vh] overflow-scroll">
+    <div className="mx-auto w-full mt-4 px-6 md:px-0 md:w-[21rem] h-[40vh] md:h-full md:max-h-[40vh] overflow-scroll">
         {data.map((item) => (
             <AccordionItems id={item.id} question={item.question} answer={item.answer} />
         ))}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,13 +2,28 @@ import Accordion from "./Accordion";
 import Image from "next/image";
 
 export default function Card() {
-    return (
-        <div className="bg-white text-dark w-full rounded-3xl">
-            <div className=" bg-mobile-pattern bg-no-repeat bg-top flex flex-col items-center justify-center pt-[8.25rem] pb-10 relative">
-                <Image src="./img/illustration-woman-online-mobile.svg" width={600} height={600} alt="illustration" className="absolute -top-[20%] w-60" />
-                <h1 className="text-black text-3xl font-semibold">FAQ</h1>
-                <Accordion />
-            </div>
+  return (
+    <div className="w-full max-w-[57.5rem] rounded-3xl bg-white text-dark md:overflow-hidden">
+      <div className="relative flex w-full flex-col items-center justify-center gap-0 bg-mobile-pattern bg-top bg-no-repeat p-0 md:flex-row md:justify-between md:gap-20 md:bg-desktop-pattern md:bg-cover md:bg-left-offset md:py-16 md:pr-28">
+        <Image
+          src="./img/illustration-woman-online-mobile.svg"
+          width={600}
+          height={600}
+          alt="illustration"
+          className="absolute -top-[20%] block w-60 md:hidden"
+        />
+        <Image
+          src="./img/illustration-woman-online-desktop.svg"
+          width={600}
+          height={600}
+          alt="illustration"
+          className="-ml-[5.25rem] hidden md:block"
+        />
+        <div className="flex w-full flex-col items-center justify-center pt-[8.25rem] pb-10 md:items-start md:py-0">
+          <h1 className="text-3xl font-semibold text-black">FAQ</h1>
+          <Accordion />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
