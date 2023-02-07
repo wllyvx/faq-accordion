7ed0d3d2a848a9da844f92452c43747f16dce359
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-07 20:03:22 +0700

    add key to accordionitems

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -6,7 +6,7 @@ export default function Accordion() {
   return (
     <div className="mx-auto w-full mt-4 px-6 md:px-0 md:w-[21rem] h-[40vh] md:h-full md:max-h-[40vh] overflow-scroll">
         {data.map((item) => (
-            <AccordionItems id={item.id} question={item.question} answer={item.answer} />
+            <AccordionItems key={item.id} id={item.id} question={item.question} answer={item.answer} />
         ))}
     </div>
   );
