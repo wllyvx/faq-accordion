89143d517c9bed53e39eec738f0b3dc59d7f79f3
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-07 19:29:05 +0700

    add orange box

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Card() {
   return (
-    <div className="w-full max-w-[57.5rem] rounded-3xl bg-white text-dark md:overflow-hidden">
+    <div className="w-full max-w-[57.5rem] rounded-3xl bg-white text-dark">
       <div className="relative flex w-full flex-col items-center justify-center gap-0 bg-mobile-pattern bg-top bg-no-repeat p-0 md:flex-row md:justify-between md:gap-20 md:bg-desktop-pattern md:bg-cover md:bg-left-offset md:py-16 md:pr-28">
         <Image
           src="./img/illustration-woman-online-mobile.svg"
@@ -18,6 +18,13 @@ export default function Card() {
           height={600}
           alt="illustration"
           className="-ml-[5.25rem] hidden md:block"
+        />
+        <Image
+          src="./img/illustration-box-desktop.svg"
+          width={600}
+          height={600}
+          alt="illustration"
+          className="absolute hidden -left-[6.8rem] top-[12.5rem] w-48 md:block"
         />
         <div className="flex w-full flex-col items-center justify-center pt-[8.25rem] pb-10 md:items-start md:py-0">
           <h1 className="text-3xl font-semibold text-black">FAQ</h1>
