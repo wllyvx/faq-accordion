3e0bab29ad40801a954d7fc9d74184927ef2a70d
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-07 08:50:56 +0700

    card illustration mobile layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,14 @@
 import Accordion from "./Accordion";
+import Image from "next/image";
 
 export default function Card() {
     return (
-        <div className="bg-white text-dark flex flex-col items-center justify-center w-full">
-            <h1 className="text-black text-3xl font-semibold">FAQ</h1>
-            <Accordion />
+        <div className="bg-white text-dark w-full rounded-3xl">
+            <div className=" bg-mobile-pattern bg-no-repeat bg-top flex flex-col items-center justify-center relative">
+                <Image src="./img/illustration-woman-online-mobile.svg" width={600} height={600} alt="illustration" className="absolute -top-[30%] w-60" />
+                <h1 className="text-black text-3xl font-semibold mt-32">FAQ</h1>
+                <Accordion />
+            </div>
         </div>
     )
 }
