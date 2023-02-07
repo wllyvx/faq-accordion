781bbf9aae2c2d8796bff89157af674fbedadde9
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-07 09:18:02 +0700

    final layout for mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Card() {
     return (
         <div className="bg-white text-dark w-full rounded-3xl">
-            <div className=" bg-mobile-pattern bg-no-repeat bg-top flex flex-col items-center justify-center relative">
-                <Image src="./img/illustration-woman-online-mobile.svg" width={600} height={600} alt="illustration" className="absolute -top-[30%] w-60" />
-                <h1 className="text-black text-3xl font-semibold mt-32">FAQ</h1>
+            <div className=" bg-mobile-pattern bg-no-repeat bg-top flex flex-col items-center justify-center pt-[8.25rem] pb-10 relative">
+                <Image src="./img/illustration-woman-online-mobile.svg" width={600} height={600} alt="illustration" className="absolute -top-[20%] w-60" />
+                <h1 className="text-black text-3xl font-semibold">FAQ</h1>
                 <Accordion />
             </div>
         </div>
