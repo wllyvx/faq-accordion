a7a1292c95f969a342755e9d000c1cec12cc03e1
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-09 09:04:09 +0700

    fixing height issue in desktop view

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -4,7 +4,7 @@ import AccordionItems from "./AccordionItems";
 
 export default function Accordion() {
   return (
-    <div className="mx-auto w-full mt-4 px-6 md:px-0 md:w-[21rem] h-[40vh] md:h-full md:max-h-[40vh] overflow-scroll">
+    <div className="mx-auto w-full mt-4 px-6 md:px-0 md:mx-0 md:w-[21rem] h-[40vh] md:h-[33vh] overflow-scroll">
         {data.map((item) => (
             <AccordionItems key={item.id} id={item.id} question={item.question} answer={item.answer} />
         ))}
